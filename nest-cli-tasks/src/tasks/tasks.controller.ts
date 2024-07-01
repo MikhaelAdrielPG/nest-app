ebbde6a7f9b17cb3f868e10b9eebf07eda32ee50
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-01 15:20:07 +0700

    Mencoba Berbagai Method Http

---
+++ nest-cli-tasks/src/tasks/tasks.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+    @Get()
+    listTasks(){
+        return 'This action returns all tasks';
+    }
+    @Post()
+    createTask(){
+        return 'This action creates a new task';
+    }
+    @Get('/:id')
+    getTask(){
+        return 'This returns a #tasks';
+    }
+}
