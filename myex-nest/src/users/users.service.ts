8269372c5efc0e7af1d0b6a81c9d4c7ec58c21b2
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-02 15:50:44 +0700

    Membuat Method Service Untuk Menghapus Data User

--- myex-nest/src/users/users.service.ts
+++ myex-nest/src/users/users.service.ts
@@ -32,4 +32,14 @@ export class UsersService {
         Object.assign(user, attrs);
         return this.userRepository.save(user);
     }
+
+    async remove(id: number) {
+        const user = await this.findOneBy(id);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return this.userRepository.remove(user);
+    }
 }
