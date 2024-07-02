f8298144007b1ae8950169da1fb6c4a9b22e6fdb
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-02 16:15:49 +0700

    Menghapus Data Yang Ada Di Database

--- myex-nest/src/users/users.controller.ts
+++ myex-nest/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -20,5 +20,10 @@ constructor(private usersService: UsersService){}
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
