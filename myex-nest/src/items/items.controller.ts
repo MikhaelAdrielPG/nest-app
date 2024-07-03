c36bc539d07ce7aeaa2a7e85f3c0c39d848482ea
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-03 12:18:50 +0700

    Menyiapkan Request Handler Item Dengan Dto-Nya

--- myex-nest/src/items/items.controller.ts
+++ myex-nest/src/items/items.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateItemDto } from './dtos/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
-  constructor(private readonly itemsService: ItemsService) {}
+  @Post()
+  createItem(@Body() body: CreateItemDto) {
+    return 'This action adds a new item';
+  }
 }
