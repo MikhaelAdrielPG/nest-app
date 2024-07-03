3784b6e8adcaa746e56f40c745fe0f1c5d679cd9
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-03 15:56:55 +0700

    Mengubah Response Create Item Dengan Class-Transform

--- myex-nest/src/items/items.controller.ts
+++ myex-nest/src/items/items.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
 import { ItemsService } from './items.service';
 import { AuthGuard } from '../guards/auth.guard';
-import { CurrentUser } from '../auth/decorators/current-user.decorators';
 import { User } from '../users/user.entity';
+import { ItemDto } from './dtos/item.dto';
+import { Serialize } from './interceptors/serialize.interceptor';
+import { CurrentUser } from '../auth/decorators/current-user.decorators';
 
 @Controller('items')
 export class ItemsController {
   constructor(private itemService: ItemsService) {}
+
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ItemDto)
   createItem(@Body() body: CreateItemDto, @CurrentUser() user: User) {
     return this.itemService.create(body, user);
   }
