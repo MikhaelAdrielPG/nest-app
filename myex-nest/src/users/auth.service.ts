99630faa798fafd2ca0c7ab4da1a5f1f11ceaebb
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-03 07:19:31 +0700

    Membuat Service Proses Login

--- myex-nest/src/users/auth.service.ts
+++ myex-nest/src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -20,10 +24,25 @@ export class AuthService {
     // hash password
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(password, salt, 64)) as Buffer;
-    const hashedPassword = salt + ':' + hash.toString('hex');
+    const hashedPassword = salt + '.' + hash.toString('hex');
 
     // create user
     const user = await this.usersService.create(name, email, hashedPassword);
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('Email tidak terdaftar');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 64)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Password salah');
+    }
 
     return user;
   }
