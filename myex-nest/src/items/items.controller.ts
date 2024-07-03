46a9a054e15c1c488c210cb7fdf664c136653923
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2024-07-03 14:46:17 +0700

    Membuat Service Create Item

--- myex-nest/src/items/items.controller.ts
+++ myex-nest/src/items/items.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
 import { ItemsService } from './items.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('items')
 export class ItemsController {
   constructor(private itemService: ItemsService) {}
   @Post()
+  @UseGuards(AuthGuard)
   createItem(@Body() body: CreateItemDto) {
     return this.itemService.create(body);
   }
